7c0ccba460dad96c0f54202cb238ab6b8e327468
deok <ziinxed@gmail.com>
2020-08-04 09:43:59 +0900

    container presenter model/ container // detail part after let see how to assign

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,45 @@
-export default () => "Home";
+import React from "react";
+import propTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({
+  nowPlaying,
+  popular,
+  upcoming,
+  loading,
+  error,
+}) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Now Playing">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Now Playing">
+          {popular.map((movie) => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: propTypes.array,
+  popular: propTypes.array,
+  upcoming: propTypes.array,
+  loading: propTypes.bool.isRequired,
+  error: propTypes.string,
+};
+
+export default HomePresenter;
